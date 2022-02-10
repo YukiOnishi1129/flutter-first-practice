9f795ee965d03f01d9ddcf864bf7ea6685625de1
yuki <yuki.onishi1129@gmail.com>
2022-02-10 17:49:51 +0900

    画像を設置

--- lib/main.dart
+++ lib/main.dart
@@ -45,21 +45,22 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('KBOYのFlutter大学'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: Text('次へ'),
-          onPressed: () {
-            // ここにボタンを押した時に呼ばれるコードを書く
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NextPage('KBOY')),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Column(
+          children:[
+            Image.network(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+            ElevatedButton(
+              child: Text('次へ'),
+              onPressed: () {
+                // ここにボタンを押した時に呼ばれるコードを書く
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage('KBOY')),
+                );
+              },
+            ),
+          ],
+        )
       ),
     );
   }
